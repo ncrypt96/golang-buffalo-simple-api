6c154b8e09ba629b965031907785cffb15699aac
Nithin Stephen <ncrypt96@gmail.com>
2020-07-18 03:42:42 +0530

    improvement(signup): extended method USignup
    added JSON Decode and loggin

--- actions/u.go
+++ actions/u.go
@@ -1,13 +1,25 @@
 package actions
 
 import (
-    "net/http"
-    
+	"encoding/json"
+	"net/http"
+	"simple_api/shapes"
+
 	"github.com/gobuffalo/buffalo"
+	log "github.com/sirupsen/logrus"
 )
 
 // USignup default implementation.
 func USignup(c buffalo.Context) error {
-	return c.Render(http.StatusOK, r.HTML("u/signup.html"))
-}
+	// declare a reference to struct
+	var s *shapes.SignUpData
+	decoder := json.NewDecoder(c.Request().Body)
+	err := decoder.Decode(&s)
+	if err != nil {
+		panic(err)
+	}
 
+	log.Info(*s)
+
+	return c.Render(http.StatusOK, r.JSON(&s))
+}
