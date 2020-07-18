d91b618b4198292cc4b4259486c1c35881b4b1ff
Nithin Stephen <ncrypt96@gmail.com>
2020-07-18 17:42:05 +0530

    docs(u.go): added docs

--- actions/u.go
+++ actions/u.go
@@ -6,8 +6,8 @@ import (
 
 	"net/http"
 	"simple_api/db"
+	"simple_api/requestshapes"
 	"simple_api/responsecodes"
-	"simple_api/shapes"
 
 	"github.com/gobuffalo/buffalo"
 	log "github.com/sirupsen/logrus"
@@ -15,12 +15,14 @@ import (
 
 // UAdd default implementation.
 func UAdd(c buffalo.Context) error {
-	var u shapes.User
+	// get a reference to the response struct
+	var u requestshapes.UserAdd
 	err := json.NewDecoder(c.Request().Body).Decode(&u)
 	if err != nil {
 		log.Error(err)
 		panic(err)
 	}
+	//If the parameters are only empty spaces
 	if strings.TrimSpace(u.Name) == "" || strings.TrimSpace(u.Quote) == "" {
 		log.Info("The request did not contain all necessary keys")
 		return c.Render(http.StatusBadRequest, r.JSON(&responsecodes.ErrResponseOnAddMissing))
@@ -36,6 +38,7 @@ func UAdd(c buffalo.Context) error {
 	return c.Render(http.StatusOK, r.JSON(&responsecodes.SuccessResponseOnAdd))
 }
 
+//addUserToDB addes user to database
 func addUserToDB(bucketName, name, quote string) error {
 	dbRef, err := db.OpenDB("./bolt.db")
 	if err != nil {
@@ -64,8 +67,8 @@ func UGet(c buffalo.Context) error {
 	}
 	{
 		val, err := getQuote("users", n)
-		log.Info("iiiiiiiiii", val, "iiiiiiiiiiiiii")
 		if err != nil {
+			// If there is an issue with the database
 			return c.Render(http.StatusBadRequest, r.JSON(&responsecodes.ErrResponseDatabase))
 		} else if len(val) <= 0 {
 			return c.Render(http.StatusConflict, r.JSON(&responsecodes.ErrResponseOnGetNotExist))
@@ -75,6 +78,7 @@ func UGet(c buffalo.Context) error {
 	}
 }
 
+//getQuote gets the quote from the database for the given name
 func getQuote(bucketName, key string) (string, error) {
 
 	dbRef, err := db.OpenDB("./bolt.db")
